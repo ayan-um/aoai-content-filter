8d1b4258c4d3f7e667b62fe1a7783a1574c92e69
Ayan <ayandsum@umich.edu>
2024-03-25 22:20:56 -0400

    Added deployment name as an input argument

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('region', help="Azure OpenAI service region")  #valid values EUS, EUS2, WUS, WUS3, NCUS
     parser.add_argument('api_ver', help="OpenAI API version") #2024-02-01
+    parser.add_argument('deployment_name', help="Model deployment name") #gpt-35-turbo-instruct-ab
     parser.add_argument('prompt', help="Prompt to test for content filtration") # 'I love sushi! '; 'I am going to cut myself! '
     parser.add_argument(
         "-v",
@@ -16,9 +17,9 @@ if __name__ == "__main__":
     
     region = args.region
     api_version = args.api_ver
+    deployment_name = args.deployment_name
     prompt = args.prompt
-    
     key, endpoint = getSecrets(region.upper())
-    resp = getAoaiResponse(prompt, key, endpoint, api_version)
+    resp = getAoaiResponse(prompt, key, endpoint, api_version, deployment_name)
     print(resp)
     
